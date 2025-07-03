7c502e077b29030f52eb3e78ad13b970d237568b
Jeffrey Arbelaez <jeffreyarbelaez@yahoo.com>
2025-07-03 18:59:19 -0400

    pipfile

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,23 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.', // copy to root of dist folder
+        },
+      ],
+    }),
+  ],
   server: {
     port: 3000,
+  },
+  build: {
+    outDir: 'dist',
   },
 })
